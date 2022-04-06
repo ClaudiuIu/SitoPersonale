d16f950c4381f2eaae2d2e5147c6fcedb842b82d
Claudiu <92544223+ClaudiuIu@users.noreply.github.com>
2022-04-06 11:08:12 +0200

    Update main.js

--- SitoPersonale/esercizoi/assets/js/main.js
+++ SitoPersonale/esercizoi/assets/js/main.js
@@ -1,10 +1,20 @@
 function inizio(){
     for(i=0;i<1000;i++){
-        if(i%10==0){    
-            numero.innerHTML += ("<span style='color: red;'>" + i + "</span>" + " ");
+        if(i%10==0){   
+            if(i%2==0) {
+                numero.innerHTML += ("<span style='color: red; font-weight: bold;'>" + i + "</span>" + " ");
+            }
+            else{
+                numero.innerHTML += ("<span style='color: red;'>" + i + "</span>" + " ");
+            }
         }
         else{
-            numero.innerHTML += ("<span style='color: black;'>" + i + "</span>" + " ");
+            if(i%2==0) {
+            numero.innerHTML += ("<span style='color: black; font-weight: bold;'>" + i + "</span>" + " ");
+            }
+            else{
+                numero.innerHTML += ("<span style='color: black;'>" + i + "</span>" + " ");
+            }
         }
 
     }
